1ff331a7945cc8fce92e3b664a897375f349c886
amirhosseinpooladi <amirhosseinpooladi@yahoo.com>
2023-08-01 20:42:59 +0430

    Updates

---
+++ _next/static/chunks/pages/footer-3a1d93b62203ee16.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[377],{205:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/footer",function(){return u(228)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=205)}),_N_E=n.O()}]);
